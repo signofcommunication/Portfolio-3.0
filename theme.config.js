07d8af41cf79a5a3f6aa4bbf8dcc93e14f61cdf7
jhvonsky <jhvonsky@gmail.com>
2023-02-19 20:06:02 +0700

    Update config file

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,8 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Jeriko Ichtus Seo. Built by My Own Mind &{' '}
+      <time>{YEAR}</time> © Jeriko Ichtus Seo.
+      <p>Built by My Own Mind & </p>
       <a href="https://nextra.site/">Nextra(MDX Support)</a>
       <style jsx>{`
         a {
